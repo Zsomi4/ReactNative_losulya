8ff6e7850c96102de36fff8c529af6a7422a5a24
Zsomi4 <k72302872172@ktch.hu>
2023-04-17 19:08:09 +0200

    Függvények és az input mezők megírása

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [chestCircumference, setChestCircumference] = useState('');
+  const [length, setLength] = useState('');
+  const [horseWeight, setHorseWeight] = useState('');
+
+  const calculateHorseWeight = () => {
+    const weight = chestCircumference * chestCircumference * length;
+    setHorseWeight(weight.toFixed(2));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Mellkas Kerülete (cm)</Text>
+      <TextInput
+        style={styles.input}
+        value={chestCircumference}
+        onChangeText={setChestCircumference}
+        keyboardType="numeric"
+      />
+
+      <Text style={styles.label}>Hossz (cm)</Text>
+      <TextInput
+        style={styles.input}
+        value={length}
+        onChangeText={setLength}
+        keyboardType="numeric"
+      />
+
+      <Button title="Számol" onPress={calculateHorseWeight} />
+
+      {horseWeight ? (
+        <Text style={styles.result}>{`Ló Súlya: ${horseWeight} kg`}</Text>
+      ) : null}
     </View>
   );
 }
